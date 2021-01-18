7a13b55cde56919de28db75a94488d3f681ad169
elizwy <elizwy@163.com>
2021-01-18 09:01:28 +0800

    选择排序

---
+++ src/main/java/com/zwy/muke/sort/SelectSort.java
@@ -0,0 +1,47 @@
+package com.zwy.muke.sort;
+
+/**
+ * 选择排序, 固定位置排序
+ */
+public  class  SelectSort<T extends Comparable> {
+    public void sort(T[] array){
+        int length=array.length;
+        for(int i=0;i<length;i++){
+            int minIndex=i;
+            for(int j=i+1;j<length;j++){
+                if(array[j].compareTo(array[minIndex])<0){
+                    minIndex=j;
+                }
+            }
+            swapElement(array,i,minIndex);
+        }
+    }
+
+    private void swapElement(T[] array,int i, int minIndex) {
+        if(i==minIndex) return;
+        T temp=array[i];
+        array[i]=array[minIndex];
+        array[minIndex]=temp;
+    }
+
+    public static void main(String[] args) {
+        Integer[] array=new Integer[]{6,-3,0,8,9,-2,5,1,7};
+        SelectSort sort = new SelectSort();
+        sort.sort(array);
+        printArray(array);
+
+
+        Double[] array1=new Double[]{6.0,-3.0,0.0,8.0,9.0,-2.0,5.0,1.0,7.0};
+        SelectSort sort1 = new SelectSort();
+        sort1.sort(array1);
+        printArray(array1);
+    }
+
+
+
+    private static <T> void printArray(T[] array) {
+        for (T element: array) {
+            System.out.println(element);
+        }
+    }
+}
