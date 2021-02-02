d1778a2990dab809e091d522e2c443bb29ef4c22
elizwy <elizwy@163.com>
2021-02-02 08:48:45 +0800

    feat: add assert

--- src/main/java/com/zwy/muke/sort/Helper/SortHelper.java
+++ src/main/java/com/zwy/muke/sort/Helper/SortHelper.java
@@ -5,6 +5,9 @@ import java.util.Random;
 public class SortHelper {
 
     public Integer[] generateArray(int size,int rangL,int rangR){
+        if(rangR<=rangL){
+            throw new RuntimeException("右区间必须大于左区间");
+        }
         Integer[] arrays = new Integer[size];
         Random random = new Random(System.currentTimeMillis());
         while (size>0){
@@ -13,7 +16,6 @@ public class SortHelper {
         }
         return arrays;
     }
-
     public static void main(String[] args) {
         SortHelper sortHelper = new SortHelper();
         Integer[] result = sortHelper.generateArray(50, 300, 400);
